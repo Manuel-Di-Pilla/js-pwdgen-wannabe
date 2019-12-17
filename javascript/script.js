daf2d1b53631a045ba73442644e4e76edf57913a
Manuel <manueldipilla3@gmail.com>
2019-12-18 00:10:57 +0100

    esercizio completo

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+var nomeUtente = prompt('come ti chiami?');
+var cognomeUtente = prompt('quale è il tuo cognome?');
+var coloreUtente = prompt('quale è il tuo colore preferito?');
+document.getElementById('generetor').innerHTML= nomeUtente + cognomeUtente + coloreUtente + "19";
